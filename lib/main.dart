61e08649329abfa064913a0c41887c98d7c6804e
Bavk <denmurdok@gmail.com>
2026-09-19 21:24:30 +0300

    feat: run packaged smoke from release executable

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/widgets.dart';
 
+import 'app/packaged_offline_smoke.dart';
 import 'app/qidi_flow_app.dart';
 
-void main() {
+Future<void> main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (PackagedOfflineSmoke.requested(args)) {
+    exit(await PackagedOfflineSmoke.run(args));
+  }
+
   runApp(const QidiFlowApp());
 }
